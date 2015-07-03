706a1965cef5855ed5249bf7a25dfa3029310d14
Ben Eggers <beggerss@cs.washington.edu>
2015-07-03 13:44:52 -0700

    done

--- Main.hs
+++ Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
-import Data.Char(chr)
+import Data.Char(chr, ord)
+import System.Environment(getArgs)
 
 
 data Command = IncrementP
@@ -19,12 +20,83 @@ data Tape a = Tape [a] a [a]
 
 type Program = Tape Command
 
+blankTape :: Tape Int
+blankTape = Tape [] 0 zeros
+                where zeros = repeat 0
 
-main = putStrLn "Hello!"
+
+-- Primary "let's do things" functions
+--
+main = do
+    args <- getArgs
+    prog <- readFile $ head args
+    runProgram (parse prog) blankTape
+
 
 runProgram :: Program -> Tape Int -> IO ()
-runProgram p t = putStrLn "Hello"
 
+runProgram p@(Tape _ IncrementC _) t = runProgram (moveRight p) (incrementCell t)
+runProgram p@(Tape _ DecrementC _) t = runProgram (moveRight p) (decrementCell t)
+runProgram p@(Tape _ IncrementP _) t = runProgram (moveRight p) (moveRight t)
+runProgram p@(Tape _ DecrementP _) t = runProgram (moveRight p) (moveRight t)
+
+runProgram p@(Tape _ Print _) t = do
+    printCell t
+    runProgram (moveRight p) t
+
+runProgram p@(Tape _ Read _) t = do
+    newT <- readCell t
+    runProgram (moveRight p) newT
+
+runProgram p@(Tape _ LoopL _) t@(Tape _ x _)
+    | x == 0    = runProgram (seekLoopR p) t
+    | otherwise = runProgram (moveRight p) t
+
+runProgram p@(Tape _ LoopR _) t@(Tape _ x _)
+    | x /= 0    = runProgram (seekLoopL p) t
+    | otherwise = runProgram (moveRight p) t
+
+-- General tape manipulation functions
+--
+moveRight :: Tape a -> Tape a
+moveRight (Tape ls c (r:rs)) = Tape (c:ls) r rs
+
+moveLeft :: Tape a -> Tape a
+moveLeft (Tape (l:ls) c rs) = Tape ls l (c:rs)
+moveLeft t = error "Already at left end of tape"
+
+
+-- Program-tape-only functions
+--
+seekLoopL :: Program -> Program
+seekLoopL t@(Tape _ LoopL _) = moveRight t
+seekLoopL t = seekLoopL $ moveLeft t
+
+seekLoopR :: Program -> Program
+seekLoopR t@(Tape _ LoopR _) = moveRight t
+seekLoopR t = seekLoopR $ moveRight t
+
+
+-- Storage-tape-only functions
+--
+incrementCell :: Tape Int -> Tape Int
+incrementCell (Tape ls c rs) = Tape ls (c + 1) rs
+
+decrementCell :: Tape Int -> Tape Int
+decrementCell (Tape _ 0 _) = error "Cannot decrement cell with value 0"
+decrementCell (Tape ls c rs) = Tape ls (c - 1) rs
+
+printCell :: Tape Int -> IO ()
+printCell (Tape _ c _) = print $ chr c
+
+readCell :: Tape Int -> IO (Tape Int)
+readCell (Tape ls _ rs) = do
+    c <- getChar
+    return (Tape ls (ord c) rs)
+
+
+-- Parsing stuff
+--
 charToCommand :: Char -> Command
 charToCommand '>' = IncrementP
 charToCommand '<' = DecrementP
@@ -40,16 +112,3 @@ parse :: String -> Program
 parse s = Tape [] c (cs ++ [End])
     where (c:cs) = filter (/= Other) $ map charToCommand s
 
-moveRight :: Tape a -> Tape a
-moveRight (Tape ls c (r:rs)) = Tape (c:ls) r rs
-
-moveLeft :: Tape a -> Tape a
-moveLeft (Tape (l:ls) c rs) = Tape ls l (c:rs)
-moveLeft t = error "Already at left end of tape"
-
-incrementCell :: Tape Int -> Tape Int
-incrementCell (Tape ls c rs) = Tape ls (c + 1) rs
-
-decrementCell :: Tape Int -> Tape Int
-decrementCell (Tape _ 0 _) = error "Cannot decrement cell with value 0"
-decrementCell (Tape ls c rs) = Tape ls (c - 1) rs
